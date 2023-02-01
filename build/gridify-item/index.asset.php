8deaaf3857b63a5ef4a390b3c7258fee9fa32a8c
Alfredo Navas <elpuas@gmail.com>
2023-01-31 23:07:09 -0600

    add grid styles

--- build/gridify-item/index.asset.php
+++ build/gridify-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7394e77db6d5f368ff87');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e5dd320d720abb92ac21');
--- build/gridify/index.asset.php
+++ build/gridify/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '521b3d3a78185352d8a9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a588537b17a017f22688');
