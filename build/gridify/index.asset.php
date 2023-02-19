132acfc97195c91e3438146591205ee9f43cadec
Alfredo Navas <elpuas@gmail.com>
2023-02-19 10:06:50 -0600

    build version

--- build/gridify/index.asset.php
+++ build/gridify/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a0d4b3bda57c69d62a6e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9e90872c240cbd5bc167');
